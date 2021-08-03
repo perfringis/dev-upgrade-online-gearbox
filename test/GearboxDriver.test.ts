28ef49be9a8f050bd9ba11ea6ed21f19f7b5f2d9
Mateusz Kopko <kopko.mateusz@gmail.com>
2021-08-03 23:24:25 +0200

    rewrite tests

--- test/GearboxDriver.test.ts
+++ test/GearboxDriver.test.ts
@@ -1,11 +1,13 @@
-import sinon, { SinonMock } from 'sinon';
+import sinon, { SinonMock, verify } from 'sinon';
 import { Characteristics } from '../src/Characteristics';
 import { ExternalSystems } from '../src/ExternalSystems';
 import { Gear } from '../src/Gear';
 import { Gearbox } from '../src/Gearbox';
 import { GearboxACL } from '../src/GearboxACL';
 import { GearboxDriver } from '../src/GearboxDriver';
+import { RPM } from '../src/RPM';
 import { RpmProvider } from '../src/RpmProvider';
+import { RpmRange } from '../src/RpmRange';
 
 describe('Test GearboxDriver class', () => {
   const characteristics = new Characteristics();
@@ -22,10 +24,32 @@ describe('Test GearboxDriver class', () => {
     characteristics
   );
 
-  test('should shift up when above max RPM', () => {
+  test('should recalculate gear in drive mode', () => {
+    const gearboxACLMock = sinon.mock(gearboxACL);
+    const characteristicsMock = sinon.mock(characteristics);
+    const rpmProviderMock = sinon.mock(rpmProvider);
+
     // given
     driver.enableDrive();
+
     // and
-    sinon.stub(gearboxACL, 'maxGear').returns(gearEight);
+    gearboxACLMock.expects('maxGear').returns(gearEight);
+    // and
+    gearboxACLMock.expects('firstGear').returns(gearOne);
+    //and
+    gearboxACLMock.expects('currentGear').returns(gearOne);
+    //and
+    characteristicsMock
+      .expects('optimalComfortRpmRange')
+      .returns(new RpmRange(RPM.k(2000), RPM.k(3000)));
+    // and
+    rpmProviderMock.expects('current').returns(RPM.k(3200));
+
+    // when
+    driver.recalculate();
+
+    //then
+    gearboxACLMock.expects('changeGearTo').withArgs(gearTwo);
+    gearboxACLMock.verify();
   });
 });
